96b0145890abe65152e45069219ab4707a4fe33f
Fixxoo <rajpalmal40@gmail.com>
2023-05-21 01:01:07 +0530

    Woling on Footer

--- src/index.js
+++ src/index.js
@@ -31,3 +31,17 @@ if(hiddenNav.classList.contains('hidden')){
   hiddenNav.classList.add("hidden")
 }
 }
+
+
+// var elem = document.querySelector('.main-carousel');
+// var flkty = new Flickity( elem, {
+//   // options
+//   cellAlign: 'left',
+//   contain: true
+// });
+
+// // element argument can be a selector string
+// //   for an individual element
+// var flkty = new Flickity( '.main-carousel', {
+//   // options
+// });
